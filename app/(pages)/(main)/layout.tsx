0d41d5a02155aad6d72de1213953e0b6228e7a2c
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-10-01 17:45:09 +0530

    left bar responsive

--- app/(pages)/(main)/layout.tsx
+++ app/(pages)/(main)/layout.tsx
@@ -9,8 +9,8 @@ import Appbar from "@/app/components/Appbar";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-screen h-screen flex flex-col">
-                <Appbar/>
-            <div className="flex-grow pl-6 pt-4 flex gap-8">
+            <Appbar />
+            <div className="pl-3 pt-2 flex gap-4 overflow-hidden">
                 <LeftBar />
                 <div className="flex-grow rounded-lg shadow-xl p-10" style={{ width: '68%' }}>
                     {children}
--- app/components/LeftBar.tsx
+++ app/components/LeftBar.tsx
@@ -1,39 +1,45 @@
-"use client"
+"use client";
 import Image from "next/image";
 import DashboardSvg from "../svg/DashbordSvg";
 import SideBar from "./SideBar";
 import LogoutButton from "./LogOutComonent";
 import { useRouter } from "next/navigation";
 
-export default function LeftBar(){
-    const router = useRouter(); 
+export default function LeftBar() {
+    const router = useRouter();
+    
     const handleNavigation = () => {
-        router.push("/err");  
+        router.push("/err");
     };
     const handleNavigationD = () => {
-        router.push("/dashboard");  
+        router.push("/dashboard");
     };
+
     return (
-        <div className="flex flex-col justify-between pb-20" style={{ width: '12%' }}>
-        <div>
-          <div onClick={handleNavigationD} className="rounded-lg bg-[#654B3E] text-white text-lg font-bold flex justify-around my-3 py-4 px-9">
-            <DashboardSvg />
-            <button>Dashboard</button>
-          </div>
-          <SideBar />
+        <div className="flex flex-col justify-between pb-4 md:w-[13.5%] w-full">
+            <div>
+                <div 
+                    onClick={handleNavigationD} 
+                    className="rounded-lg bg-[#654B3E] mx-4 text-white text-lg font-bold flex justify-around my-3 py-4 px-4 md:px-9 cursor-pointer"
+                >
+                    <DashboardSvg />
+                    <button className="hidden lg:block">Dashboard</button>
+                </div>
+                <SideBar />
+            </div>
+
+            <div>
+                <Image
+                    src="/Support.png"
+                    layout="responsive"
+                    onClick={handleNavigation}
+                    width={271}
+                    height={300}
+                    alt="Background"
+                    className="object-cover cursor-pointer p-5"
+                />
+                <LogoutButton />
+            </div>
         </div>
-        <div className="pb-10">
-          <Image
-            src="/Support.png"
-            layout="responsive"
-            onClick={handleNavigation}
-            width={271}
-            height={300}
-            alt="Background"
-            className="object-cover pb-10 cursor-pointer"
-          />
-          <LogoutButton />
-        </div>
-      </div>
-    )
+    );
 }
